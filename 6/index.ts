f18ad7172abd6383780b3939561775b0510629d0
Derek Surguy <surguy.derek@startmail.com>
2026-01-02 13:02:49 -0600

    Redo day 6 part 1 by pre-processing actions to determine max input lengths (prep for part 2)

--- 6/index.ts
+++ 6/index.ts
@@ -19,32 +19,38 @@ const { values: runArgs } = parseArgs({
 })
 
 const input = Bun.file(resolve(__dirname, runArgs.example ? 'example' : 'input'));
-const lines = (await input.text()).split('\n').filter(v => v).map(v => v.split(/\s+/g).filter(v => v))
+const lines = (await input.text()).split('\n').filter(v => v);
+const actionLine = lines[lines.length-1]!;
+const actions = actionLine.split(/\s(?=[*+])/g).map(v => ({
+  length: v.length,
+  action: v[0]!,
+}));
 
 const time = performance.now();
-
-const actions = lines[lines.length-1]!;
 const results: number[] = [];
 
-for( let lineIndex = 0; lineIndex < lines.length-1; lineIndex++) {
-  const line = lines[lineIndex]!;
-  for ( let i=0; i<line.length; i++ ) {
-    const value = Number(line[i]);
-    if( !results[i] ) {
-      results[i] = value;
+let currentIndex = 0;
+for ( let action of actions ) {
+  let result = 0;
+  for( let lineIndex = 0; lineIndex < lines.length-1; lineIndex++) {
+    const line = lines[lineIndex]!;
+    const value = Number(line.substring(currentIndex, currentIndex + action.length));
+    if( !result ) {
+      result = value;
     }
-    else if( actions[i] === '*' ) {
-      results[i] = results[i]! * value
+    else if( action.action === '*' ) {
+      result *= value
     } else { // +
-      results[i] = results[i]! + value
+      result += value
     }
-    if( isNaN(Number(results[i])) ) {
-      console.log(`Problem at line ${lineIndex}, col ${i}`)
+    if( isNaN(Number(result)) ) {
+      console.log(`Problem at line ${lineIndex}, col ${currentIndex}`)
       process.exit(0)
     }
   }
+  results.push(result);
+  currentIndex += action.length + 1
 }
-
 const total = results.reduce((acc, v) => acc + v, 0)
 
 const duration = performance.now() - time
